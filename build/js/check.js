770fc89f21eaeeb5db3df2e34dca57f7e74f586c
Alex Vimble <alex.vimble@gmail.com>
2016-08-07 19:44:24 +0300

    Fixed cycles and messages with vars

--- build/js/check.js
+++ build/js/check.js
@@ -1,15 +1,23 @@
 function getMessage (a, b) {
 
   if (a === true) {
-    return "Я попал в [b]";
+    return "Я попал в " + b;
   } else if (a === false) {
     return "Я никуда не попал";
   } else if (typeof a === "number") {
-    return "Я прыгнул на [a] * 100 сантиметров";
+    return "Я прыгнул на " + a * 100 + " сантиметров";
   } else if (Array.isArray(a) && Array.isArray(b)) {
-    return "Я прошёл [distancePath] метров";
+    var distancePath = 0;
+    for (var i = 0; i < a.length; i++) {
+      distancePath += a[i]*b[i];
+    }
+    return "Я прошёл " + distancePath + " метров";
   } else if (Array.isArray(a)) {
-    return "Я прошёл [numberOfSteps] шагов"
+    var numberOfSteps = 0;
+    for (var i = 0; i < a.length; i++) {
+      numberOfSteps += a[i];
+    }
+    return "Я прошёл " + numberOfSteps + " шагов";
   }
 
 };
